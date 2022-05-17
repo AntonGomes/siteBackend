ecfb98ef7048cca85d336e21fa391525ffa82c0a
AntonGomes <aomlgomes@gmail.com>
2022-05-17 16:38:24 +0100

    initial commit

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+import json
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+def writeToFile(url):
+    file = open("urls.json", 'r+', encoding='UTF-8')
+    data = json.load(file)
+    data["urls"].append(url)
+    file.seek(0)
+    json.dump(data, file)
+
+
+
+def readFromFile():
+    file = open("urls.json", 'r+')
+    data = json.load(file)
+    return data
+
+@app.route("/setUrls")
+@cross_origin()
+def setUrls():
+    url = request.args["url"]
+    writeToFile(url)
+    return readFromFile()
+
+@app.route("/")
+@cross_origin()
+def getUrls():
+    return readFromFile()
+
+
